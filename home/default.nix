3391bad79acc26219fd8ebebe36eee9971afb6d5
Cédric Meukens <cedricmeukens@Cedrics-MBP-2.localdomain>
2024-02-07 18:43:43 +0100

    removed neovim

--- home/default.nix
+++ home/default.nix
@@ -1,11 +1,8 @@
 { flake, ... }:
 {
   imports = [
-    # This loads ./neovim/default.nix - neovim configured for Haskell dev, and other things.
-    ./neovim
     ./starship.nix
     ./terminal.nix
-    # Add more of your home-manager modules here.
   ];
 
   # Recommended Nix settings
