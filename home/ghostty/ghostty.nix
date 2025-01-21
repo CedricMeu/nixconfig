59833f491eb152f8ace8569b9ff33318164b727b
CedricMeu <5839757+CedricMeu@users.noreply.github.com>
2025-01-21 13:18:26 +0100

    Ghostty config, additional helix config

---
+++ home/ghostty/ghostty.nix
@@ -0,0 +1,10 @@
+{ pkgs, config, ... }:
+let
+  ghostty_config = builtins.readFile ./config;
+in
+{
+  home.file."${config.xdg.configHome}/ghostty/config" =
+    {
+      text = builtins.replaceStrings [ "ZELLIJ_PATH" ] [ "${pkgs.zellij.outPath}/bin/zellij" ] ghostty_config;
+    };
+}
