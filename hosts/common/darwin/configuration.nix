b11dc6010aae36a41990ee87fb8237635cdb5203
CedricMeu <5839757+CedricMeu@users.noreply.github.com>
2025-05-26 10:26:08 +0200

    Fixes

--- hosts/common/darwin/configuration.nix
+++ hosts/common/darwin/configuration.nix
@@ -4,6 +4,8 @@
   networking.computerName = hostname;
   system.defaults.smb.NetBIOSName = hostname;
   system.stateVersion = 5;
+  # Temporary fix
+  system.primaryUser = "${username}";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."${username}" = {
